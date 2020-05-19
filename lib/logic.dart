5ebf70f0c8e440916b3481f5401c9127c600800c
Carl Chan <25685940+Carlchk@users.noreply.github.com>
2020-05-19 18:34:34 +0800

    Remove trash

--- lib/logic.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:user_location/user_location.dart';
-
-import 'model/geomodel.dart';
-
-class Logic with ChangeNotifier {
-  
-}
